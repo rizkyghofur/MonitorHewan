2dda83110b1a6f57a35abcd805d5a5316b82499c
rizkyghofur <rizky.abdulghofur@gmail.com>
2019-09-13 06:28:00 +0700

    Commit awal pembuatan project

---
+++ app/src/main/java/com/rizkyghofur/monitorhewan/Koordinat.java
@@ -0,0 +1,14 @@
+package com.rizkyghofur.monitorhewan;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class Koordinat extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_koordinat);
+    }
+}
---
+++ app/src/main/java/com/rizkyghofur/monitorhewan/MonitorActivity.java
@@ -0,0 +1,14 @@
+package com.rizkyghofur.monitorhewan;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MonitorActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_monitor);
+    }
+}
