08999a36761297d2087e7222cbe9837a04b07ea7
rizkyghofur <rizky.abdulghofur@gmail.com>
2019-09-26 22:09:00 +0700

    Perubahan pada activity Detak Jantung di mana activity Kelembapan tidak dibutuhkan

---
+++ app/src/main/java/com/rizkyghofur/monitorhewan/DetakJantung.java
@@ -0,0 +1,14 @@
+package com.rizkyghofur.monitorhewan;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class DetakJantung extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_detak_jantung);
+    }
+}
