88f7c527e1d10bf50b59a0b3f36cf31a439a4f99
rizkyghofur <rizky.abdulghofur@gmail.com>
2019-09-27 16:34:06 +0700

    Penambahan tampilan release data dari firebase pada MonitorActivity

--- app/src/main/java/com/rizkyghofur/monitorhewan/MonitorActivity.java
+++ app/src/main/java/com/rizkyghofur/monitorhewan/MonitorActivity.java
@@ -1,14 +1,94 @@
 package com.rizkyghofur.monitorhewan;
 
 import androidx.appcompat.app.AppCompatActivity;
-
+import androidx.core.content.ContextCompat;
+import androidx.recyclerview.widget.DividerItemDecoration;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
 import android.os.Bundle;
+import android.os.Handler;
+import android.util.Log;
+import android.widget.Toast;
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
 
-public class MonitorActivity extends AppCompatActivity {
+import java.util.ArrayList;
+
+public class MonitorActivity extends AppCompatActivity{
+    private RecyclerView recyclerView;
+    private RecyclerView.Adapter adapter;
+    private RecyclerView.LayoutManager layoutManager;
+    private DatabaseReference databaseReference;
+    private ArrayList<DatabaseHelper> listData;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_monitor);
+        recyclerView = findViewById(R.id.datalist);
+        MyRecyclerView();
+        GetData();
+    }
+
+    private void GetData(){
+        Toast.makeText(getApplicationContext(),"Mohon Tunggu Sebentar...", Toast.LENGTH_LONG).show();
+        databaseReference = FirebaseDatabase.getInstance().getReference();
+        databaseReference.child("monitorhewan")
+                .addValueEventListener(new ValueEventListener() {
+                    @Override
+                    public void onDataChange(DataSnapshot dataSnapshot) {
+                        listData = new ArrayList<>();
+
+                        for (DataSnapshot snapshot : dataSnapshot.getChildren()){
+                            DatabaseHelper data = snapshot.getValue(DatabaseHelper.class);
+                            listData.add(data);
+                        }
+                        adapter = new RecyclerViewAdapter(listData);
+                        recyclerView.setAdapter(adapter);
+                        Toast.makeText(getApplicationContext(),"Data Berhasil Dimuat", Toast.LENGTH_LONG).show();
+                    }
+
+                    @Override
+                    public void onCancelled(DatabaseError databaseError) {
+                        Toast.makeText(getApplicationContext(),"Data Gagal Dimuat", Toast.LENGTH_LONG).show();
+                        Log.e("MyListActivity", databaseError.getDetails()+" "+databaseError.getMessage());
+                    }
+
+                });
+    }
+
+    public boolean doubleTap = false;
+    @Override
+    public void onBackPressed(){
+        if(doubleTap){
+            super.onBackPressed();
+            return;
+        }
+
+        this.doubleTap = true;
+        Toast.makeText(this, "Ketuk kembali 2 kali untuk keluar", Toast.LENGTH_SHORT).show();
+
+        new Handler().postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                doubleTap = false;
+            }
+        }, 2000);
+    }
+
+    //Methode yang berisi kumpulan baris kode untuk mengatur RecyclerView
+    private void MyRecyclerView(){
+        //Menggunakan Layout Manager, Dan Membuat List Secara Vertical
+        layoutManager = new LinearLayoutManager(this);
+        recyclerView.setLayoutManager(layoutManager);
+        recyclerView.setHasFixedSize(true);
+
+        //Membuat Underline pada Setiap Item Didalam List
+        DividerItemDecoration itemDecoration = new DividerItemDecoration(getApplicationContext(), DividerItemDecoration.VERTICAL);
+        itemDecoration.setDrawable(ContextCompat.getDrawable(getApplicationContext(), R.drawable.line));
+        recyclerView.addItemDecoration(itemDecoration);
     }
 }
