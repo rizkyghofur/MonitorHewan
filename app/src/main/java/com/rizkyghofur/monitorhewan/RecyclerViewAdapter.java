89bdc76b4cccd3c0b1e2bfe21b5bbdc24da53f37
Hendradev <43094782+Hendradev@users.noreply.github.com>
2019-10-03 01:19:15 +0700

    Menambahkan fungsi intent atau pindah ke lain class (button ke grafik) sesuai dengan indeksnya.
    Contohnya ada di button suhu (btnSuhu) pada baris 45 untuk mengambil data yang dikirim. Kebetulan data yang dikirim hanya tunggal, bisa aja diganti ke tipe Array atau list.

--- app/src/main/java/com/rizkyghofur/monitorhewan/RecyclerViewAdapter.java
+++ app/src/main/java/com/rizkyghofur/monitorhewan/RecyclerViewAdapter.java
@@ -2,10 +2,15 @@ package com.rizkyghofur.monitorhewan;
 
 import android.annotation.SuppressLint;
 import android.content.Context;
+import android.content.Intent;
 import android.view.LayoutInflater;
 import android.view.View;
+import android.view.View.OnClickListener;
 import android.view.ViewGroup;
+import android.widget.Button;
 import android.widget.TextView;
+import android.widget.Toast;
+
 import androidx.annotation.NonNull;
 import androidx.recyclerview.widget.RecyclerView;
 import java.util.ArrayList;
@@ -50,6 +55,8 @@ public class RecyclerViewAdapter extends RecyclerView.Adapter<RecyclerViewAdapte
 
         private TextView Tanggal, Id, Suhu, DetakJantung, Latlong;
 
+        private Button btnSuhu, btnJantung, btnMap;
+
         ViewHolder(View itemView) {
             super(itemView);
             Tanggal = itemView.findViewById(R.id.tgl);
@@ -57,6 +64,44 @@ public class RecyclerViewAdapter extends RecyclerView.Adapter<RecyclerViewAdapte
             Suhu = itemView.findViewById(R.id.suhu);
             DetakJantung = itemView.findViewById(R.id.dtkjtg);
             Latlong = itemView.findViewById(R.id.latlong);
+
+            btnSuhu = itemView.findViewById(R.id.gfsuhu);
+            btnJantung = itemView.findViewById(R.id.gfkdtkjtg);
+            btnMap = itemView.findViewById(R.id.gfklatlong);
+
+            btnSuhu.setOnClickListener(new OnClickListener() {
+                @Override
+                public void onClick(View v) {
+                    int position = getAdapterPosition();
+
+                    Toast.makeText(v.getContext(),"Button Suhu "+position, Toast.LENGTH_LONG).show();
+
+                    Context context = v.getContext();
+                    int SuhuBaru = listData.get(position).getSuhu();
+                    Intent intent = new Intent(context, Suhu.class);
+                    intent.putExtra("nilai_suhu", SuhuBaru);
+                    intent.putExtra("nilai_X", "A");
+                    context.startActivity(intent);
+                }
+            });
+
+            btnJantung.setOnClickListener(new OnClickListener() {
+                @Override
+                public void onClick(View v) {
+                    int position = getAdapterPosition();
+
+                    Toast.makeText(v.getContext(),"Button Jantung "+position, Toast.LENGTH_LONG).show();
+                }
+            });
+
+            btnMap.setOnClickListener(new OnClickListener() {
+                @Override
+                public void onClick(View v) {
+                    int position = getAdapterPosition();
+
+                    Toast.makeText(v.getContext(),"Button Jantung "+position, Toast.LENGTH_LONG).show();
+                }
+            });
         }
     }
 }
